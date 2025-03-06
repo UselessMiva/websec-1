9c81071bbee01ed54b356076ba17c475e7a573be
H0r0sho <undeaddragon163rus@gmail.com>
2025-03-06 21:49:18 +0400

    done

---
+++ index.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const calcButton = document.querySelector('.calc-result');
+    calcButton && calcButton.addEventListener('click', onClickHandler);
+
+    const prevResultElem = document.querySelector(".prev-result");
+
+    if (prevResultElem && window.localStorage && window.localStorage.getItem('previousResult'))
+        prevResultElem.innerHTML = window.localStorage.getItem('previousResult');
+})
+
+function setRes(result) {
+    const prevResultElem = document.querySelector(".prev-result");
+    const currResultElem = document.querySelector(".current-result");
+    if (prevResultElem && currResultElem) {
+        if (currResultElem.innerHTML) prevResultElem.innerHTML = currResultElem.innerHTML;
+        currResultElem.innerHTML = result;
+        window.localStorage && window.localStorage.setItem('previousResult', result);
+    }
+}
+
+function onClickHandler() {
+    const firstNumElem = document.querySelector("#first-number");
+    const secondNumElem = document.querySelector("#second-number");
+    const operationElem = document.querySelector(".select-operation");
+
+    if (firstNumElem
+        && secondNumElem
+        && operationElem
+    ) {
+
+        const firstNumber = Number.parseFloat(firstNumElem.value);
+        const secondNumber = Number.parseFloat(secondNumElem.value);
+
+        if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+            const operation = operationElem.value;
+
+            if (operation === "/" || operation === "%") {
+                if (secondNumber === 0) {
+                    alert("Деление на 0");
+                    return;
+                }
+            }
+            let result = "";
+            switch (operation) {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "∗":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+                case "%":
+                    result = firstNumber % secondNumber;
+                    break;
+                case "^":
+                    result = firstNumber ** secondNumber;
+                    break;
+            }
+            const resultStr = operation === '/' && result % 1 !== 0
+                ? `${firstNumber} ${operation} ${secondNumber} = ${result.toFixed(6)}` 
+                : `${firstNumber} ${operation} ${secondNumber} = ${Number(result)}`;
+            setRes(resultStr);
+        }
+        else {
+            alert('Заполните все поля!')
+        }
+
+    }
+}
+
